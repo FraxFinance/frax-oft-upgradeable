f8cdb55fca5378dc196f4f729d27bc08803eafcb
Carter Carlson <pegahcarter@gmail.com>
2024-06-12 09:18:29 -0700

    Feat: deploy Frax Ecosystem OFTs (#1)
    * build: fraxfinance/layerzero-v2-upgradeable
    
    * feat: FraxOFTUpgradeable
    
    * build: fix
    
    * fix(deploy): deployment addr/order
    
    * refactor: deployFraxOFTUpgradeablesAndProxies
    
    * feat: setPeers
    
    * feat: admin role setup
    
    * refactor: bytes conversion, initial state check
    
    * feat: ProxyAdmin
    
    * feat: setEnforcedOptions
    
    * feat: set DVNs
    
    * fix: build
    
    * pass until setDVNs()
    
    * feat: setSendReceiveLibraries()
    
    * feat: deploy local passes
    
    * docs: notice on resolving failed `setSendAndReceiveLibrary()`
    
    * fix: use default send/receive lib
    
    * fix: blast EID
    
    * build: deploy addrs
    
    * deploy: final script for Mode
    
    * ops: deploy v1.0.0
    
    * fix: deterministic ProxyAdmin
    
    * feat: deterministic proxy addr
    
    * feat: ImplementationMock
    
    * ops: deploy v0.1.0

---
+++ scripts/DeployFraxOFTProtocol.s.sol
@@ -0,0 +1,360 @@
+// SPDX-License-Identifier: ISC
+pragma solidity ^0.8.19;
+
+// TODO: package names under same base dir
+import { Script } from "forge-std/Script.sol";
+import "frax-template/src/Constants.sol";
+import { FraxOFTUpgradeable } from "contracts/FraxOFTUpgradeable.sol";
+import { ImplementationMock } from "contracts/mocks/ImplementationMock.sol";
+import { ProxyAdmin, TransparentUpgradeableProxy } from "@fraxfinance/layerzero-v2-upgradeable/messagelib/contracts/upgradeable/proxy/ProxyAdmin.sol";
+import { Ownable } from "@openzeppelin/contracts/access/Ownable.sol";
+import { EndpointV2 } from "@fraxfinance/layerzero-v2-upgradeable/protocol/contracts/EndpointV2.sol";
+import { OptionsBuilder } from "@fraxfinance/layerzero-v2-upgradeable/oapp/contracts/oapp/libs/OptionsBuilder.sol";
+import { SetConfigParam, IMessageLibManager} from "@fraxfinance/layerzero-v2-upgradeable/protocol/contracts/interfaces/IMessageLibManager.sol";
+import { UlnConfig } from "@fraxfinance/layerzero-v2-upgradeable/messagelib/contracts/uln/UlnBase.sol";
+import { EnforcedOptionParam } from "@fraxfinance/layerzero-v2-upgradeable/oapp/contracts/oapp/interfaces/IOAppOptionsType3.sol";
+import { Constant } from "@fraxfinance/layerzero-v2-upgradeable/messagelib/test/util/Constant.sol";
+
+/*
+TODO
+- Existing chains
+    - setProxyPeers()
+    - setEnforcedOptions()
+    - setDVNs()
+- Setup frxETH, FPI with L0 mainnet adapter & legacies
+- Proxy EIDs
+
+Required DVNs:
+- L0
+- Horizon
+- example: https://explorer.metis.io/tx/0x465d4dac4e7bc8cb9c8be8908aac5bdc00b4bc64988f82c1e46c10b3c93829e5/eventlog
+
+Mainnet Addresses
+- FXSOFTAdapter:        0x23432452B720C80553458496D4D9d7C5003280d0
+- sFRAXOFTAdapter:      0xe4796cCB6bB5DE2290C417Ac337F2b66CA2E770E
+- sfrxETHOFTAdapter:    0x1f55a02A049033E3419a8E2975cF3F572F4e6E9A
+- FRAXOFTAdapter:       0x909DBdE1eBE906Af95660033e478D59EFe831fED
+    > Has no TVL / txs
+    > I have not confirmed DVNs
+*/
+
+
+contract DeployFraxOFTProtocol is Script {
+    using OptionsBuilder for bytes;
+
+    uint256 public oftDeployerPK = vm.envUint("PK_OFT_DEPLOYER");
+    uint256 public configDeployerPK = vm.envUint("PK_CONFIG_DEPLOYER");
+
+    // TODO: load these dynamically in setUp based on chainId
+    address public delegate;
+    address public endpoint;
+    address public receiveLib302;
+    address public sendLib302;
+    // https://docs.layerzero.network/v2/developers/evm/technical-reference/dvn-addresses#layerzero-labs
+    address public dvnHorizon;
+    address public dvnL0;
+
+    // Mock implementation used to enable pre-determinsitic proxy creation
+    address public implementationMock;
+
+    // Deployed proxies
+    address public proxyAdmin;
+    address public fxsOft;
+    address public sFraxOft;
+    address public sfrxEthOft;
+    address public fraxOft;
+
+    /// EIDs- each value should be unique
+    uint32[] public legacyEids;
+    uint32[] public proxyEids; // TODO: setup
+    
+    // 1:1 match between these arrays for setting peers
+    address[] public legacyOfts;
+    address[] public proxyOfts;
+
+    EnforcedOptionParam[] public enforcedOptionsParams;
+
+    SetConfigParam[] public setConfigParams;
+
+    uint256 public chainId;
+    // string public rpc;
+
+    function version() public view returns (uint256, uint256, uint256) {
+        return (0, 1, 0);
+    }
+
+    modifier broadcastAs(uint256 privateKey) {
+        vm.startBroadcast(privateKey);
+        _;
+        vm.stopBroadcast();
+    }
+
+    function setUp() external {
+        // Set constants based on deployment chain id
+        chainId = block.chainid;
+        if (chainId == 1) {
+            // TODO: configure constants with these addrs
+            // delegate = Constants.Mainnet.TIMELOCK_ADDRESS;
+            // endpoint = Constants.Mainnet.L0_ENDPOINT; // TODO: may / may not vary
+        } else if (chainId == 34443) {
+            // mode
+            delegate = 0x6336CFA6eDBeC2A459d869031DB77fC2770Eaa66;
+            endpoint = 0x1a44076050125825900e736c501f859c50fE728c;
+            receiveLib302 = 0xc1B621b18187F74c8F6D52a6F709Dd2780C09821;
+            sendLib302 = 0x2367325334447C5E1E0f1b3a6fB947b262F58312;
+            dvnHorizon = 0xaCDe1f22EEAb249d3ca6Ba8805C8fEe9f52a16e7;
+            dvnL0 = 0xce8358bc28dd8296Ce8cAF1CD2b44787abd65887;
+        }
+        require(delegate != address(0), "Empty delegate");
+        require(endpoint != address(0), "Empty endpoint");
+
+        /// @dev: this array maintains the same token order as proxyOfts and the addrs are confirmed on eth mainnet.
+        legacyOfts.push(0x23432452B720C80553458496D4D9d7C5003280d0); // fxs
+        legacyOfts.push(0xe4796cCB6bB5DE2290C417Ac337F2b66CA2E770E); // sFRAX
+        legacyOfts.push(0x1f55a02A049033E3419a8E2975cF3F572F4e6E9A); // sfrxETH
+        legacyOfts.push(0x909DBdE1eBE906Af95660033e478D59EFe831fED); // FRAX
+
+        /// @dev EIDs from https://docs.layerzero.network/v2/developers/evm/technical-reference/deployed-contracts
+        legacyEids.push(uint32(30101)); // ethereum
+        legacyEids.push(uint32(30151)); // metis
+        legacyEids.push(uint32(30184)); // base
+        legacyEids.push(uint32(30243)); // blast
+    }
+
+    function run() external {
+        preDeployChecks();
+        deployFraxOFTUpgradeablesAndProxies();
+        setLegacyPeers();
+        setProxyPeers();
+        setEnforcedOptions();
+        setDVNs();
+        setPriviledgedRoles();
+    }
+
+    function preDeployChecks() public view {
+        // legacy EIDs
+        for (uint256 e=0; e<legacyEids.length; e++) {
+            require(
+                IMessageLibManager(endpoint).isSupportedEid(legacyEids[e]),
+                "L0 team required to setup `defaultSendLibrary` and `defaultReceiveLibrary` for EID"
+            );
+        }
+        // proxy EIDs
+        for (uint256 e=0; e<proxyEids.length; e++) {
+            require(
+                IMessageLibManager(endpoint).isSupportedEid(proxyEids[e]),
+                "L0 team required to setup `defaultSendLibrary` and `defaultReceiveLibrary` for EID"
+            );
+        }
+    }
+
+    function setDVNs() broadcastAs(configDeployerPK) public {
+        UlnConfig memory ulnConfig;
+        ulnConfig.requiredDVNCount = 2;
+        address[] memory requiredDVNs = new address[](2);
+
+        // sort in ascending order (as spec'd in UlnConfig)
+        if (uint160(dvnHorizon) < uint160(dvnL0)) {
+            requiredDVNs[0] = dvnHorizon;
+            requiredDVNs[1] = dvnL0;
+        } else {
+            requiredDVNs[0] = dvnL0;
+            requiredDVNs[1] = dvnHorizon;
+        }
+        ulnConfig.requiredDVNs = requiredDVNs;
+
+        // push config for legacy OFTs
+        for (uint256 e=0; e<legacyEids.length; e++) {
+            setConfigParams.push(
+                SetConfigParam({
+                    eid: legacyEids[e],
+                    configType: Constant.CONFIG_TYPE_ULN,
+                    config: abi.encode(ulnConfig)                    
+                })
+            );
+        }
+
+        for (uint256 e=0; e<proxyEids.length; e++) {
+            setConfigParams.push(
+                SetConfigParam({
+                    eid: proxyEids[e],
+                    configType: Constant.CONFIG_TYPE_ULN,
+                    config: abi.encode(ulnConfig)
+                })
+            );
+        }
+
+        // Submit txs to setup DVN on source chain
+        for (uint256 i=0; i<proxyOfts.length; i++) {
+            IMessageLibManager(endpoint).setConfig({
+                _oapp: proxyOfts[i],
+                _lib: receiveLib302,
+                _params: setConfigParams
+            });
+            IMessageLibManager(endpoint).setConfig({
+                _oapp: proxyOfts[i],
+                _lib: sendLib302,
+                _params: setConfigParams
+            });
+        }
+    }
+
+    function setPriviledgedRoles() broadcastAs(configDeployerPK) public {
+        /// @dev transfer ownership of OFT
+        for (uint256 i=0; i<proxyOfts.length; i++) {
+            address proxyOft = proxyOfts[i];
+            FraxOFTUpgradeable(proxyOft).setDelegate(delegate);
+            Ownable(proxyOft).transferOwnership(delegate);
+        }
+
+        /// @dev transfer ownership of ProxyAdmin
+        Ownable(proxyAdmin).transferOwnership(delegate);
+    }
+
+    function setEnforcedOptions() broadcastAs(configDeployerPK) public {
+        // For each peer, default 
+        // https://github.com/FraxFinance/LayerZero-v2-upgradeable/blob/e1470197e0cffe0d89dd9c776762c8fdcfc1e160/oapp/test/OFT.t.sol#L417
+        bytes memory optionsTypeOne = OptionsBuilder.newOptions().addExecutorLzReceiveOption(200_000, 0);
+        bytes memory optionsTypeTwo = OptionsBuilder.newOptions().addExecutorLzReceiveOption(250_000, 0);
+
+        // legacy eids
+        for (uint256 e=0; e<legacyEids.length; e++) {
+            uint32 eid = legacyEids[e];
+            enforcedOptionsParams.push(EnforcedOptionParam(eid, 1, optionsTypeOne));
+            enforcedOptionsParams.push(EnforcedOptionParam(eid, 2, optionsTypeTwo));
+        }
+
+        for (uint256 p=0; p<proxyEids.length; p++) {
+            uint32 eid = proxyEids[p];
+            enforcedOptionsParams.push(EnforcedOptionParam(eid, 1, optionsTypeOne));
+            enforcedOptionsParams.push(EnforcedOptionParam(eid, 2, optionsTypeTwo));
+        }
+
+        for (uint256 i=0; i<proxyOfts.length; i++) {
+            FraxOFTUpgradeable(proxyOfts[i]).setEnforcedOptions(enforcedOptionsParams);
+        }
+    }
+
+    /// @dev legacy, non-upgradeable OFTs
+    function setLegacyPeers() public broadcastAs(configDeployerPK) {
+        for (uint256 i=0; i<proxyOfts.length; i++) {
+            address proxyOft = proxyOfts[i];
+            address legacyOft = legacyOfts[i];
+            for (uint256 e=0; e<legacyEids.length; e++) {
+                FraxOFTUpgradeable(proxyOft).setPeer(legacyEids[e], addressToBytes32(legacyOft));
+            }
+        }
+    }
+
+    /// @dev Upgradeable OFTs maintaining the same address cross-chain.
+    function setProxyPeers() public broadcastAs(configDeployerPK) {
+        for (uint256 i=0; i<proxyOfts.length; i++) {
+            address proxyOft = proxyOfts[i];
+            for (uint256 p=0; i<proxyEids.length; p++) {
+                FraxOFTUpgradeable(proxyOft).setPeer(proxyEids[p], addressToBytes32(proxyOft));
+            }
+        }
+    }
+
+
+    // TODO: missing ecosystem tokens (FPI, etc.)
+    function deployFraxOFTUpgradeablesAndProxies() broadcastAs(oftDeployerPK) public {
+
+        // Proxy admin
+        proxyAdmin = address(new ProxyAdmin(vm.addr(configDeployerPK)));
+
+        // Implementation mock
+        implementationMock = address(new ImplementationMock());
+
+        /// @dev: follows deployment order of legacy OFTs found at https://etherscan.io/address/0xded884435f2db0169010b3c325e733df0038e51d
+        // Deploy FXS
+        (,fxsOft) = deployFraxOFTUpgradeableAndProxy({
+            _name: "Frax Share",
+            _symbol: "FXS"
+        });
+
+        // Deploy sFRAX
+        (,sFraxOft) = deployFraxOFTUpgradeableAndProxy({
+            _name: "Staked Frax",
+            _symbol: "sFRAX"
+        });
+
+        // sfrxETH
+        (,sfrxEthOft) = deployFraxOFTUpgradeableAndProxy({
+            _name: "Staked Frax Ether",
+            _symbol: "sfrxETH"
+        });
+
+        // Deploy FRAX
+        (,fraxOft) = deployFraxOFTUpgradeableAndProxy({
+            _name: "Frax",
+            _symbol: "FRAX"
+        });
+
+        // Deploy FPI
+    }
+
+    /// @notice Sourced from https://github.com/FraxFinance/LayerZero-v2-upgradeable/blob/e1470197e0cffe0d89dd9c776762c8fdcfc1e160/oapp/test/TestHelper.sol#L266
+    ///     With state checks
+    function deployFraxOFTUpgradeableAndProxy(
+        string memory _name,
+        string memory _symbol
+    ) public returns (address implementation, address proxy) {
+        bytes memory bytecode = bytes.concat(
+            abi.encodePacked(type(FraxOFTUpgradeable).creationCode),
+            abi.encode(endpoint)
+        );
+        assembly {
+            implementation := create(0, add(bytecode, 0x20), mload(bytecode))
+            if iszero(extcodesize(implementation)) {
+                revert(0, 0)
+            }
+        }
+        /// @dev: config deployer is temporary OFT owner until setPriviledgedRoles()
+        bytes memory initializeArgs = abi.encodeWithSelector(
+            FraxOFTUpgradeable.initialize.selector,
+            _name,
+            _symbol,
+            vm.addr(configDeployerPK)
+        );
+
+        /// @dev: create pre-deterministic proxy address, then initialize with correct implementation
+        proxy = address(new TransparentUpgradeableProxy(implementationMock, vm.addr(oftDeployerPK), ""));
+        TransparentUpgradeableProxy(payable(proxy)).upgradeToAndCall({
+            newImplementation: implementation,
+            data: initializeArgs
+        });
+        TransparentUpgradeableProxy(payable(proxy)).changeAdmin(proxyAdmin);
+        proxyOfts.push(proxy);
+
+        // State checks
+        require(
+            isStringEqual(FraxOFTUpgradeable(proxy).name(), _name),
+            "OFT name incorrect"
+        );
+        require(
+            isStringEqual(FraxOFTUpgradeable(proxy).symbol(), _symbol),
+            "OFT symbol incorrect"
+        );
+        require(
+            address(FraxOFTUpgradeable(proxy).endpoint()) == endpoint,
+            "OFT endpoint incorrect"
+        );
+        require(
+            EndpointV2(endpoint).delegates(proxy) == vm.addr(configDeployerPK),
+            "Endpoint delegate incorrect"
+        );
+        require(
+            FraxOFTUpgradeable(proxy).owner() == vm.addr(configDeployerPK),
+            "OFT owner incorrect"
+        );
+    }
+
+    function isStringEqual(string memory _a, string memory _b) internal pure returns (bool) {
+        return keccak256(abi.encodePacked(_a)) == keccak256(abi.encodePacked(_b));
+    }
+
+    function addressToBytes32(address _addr) internal pure returns (bytes32) {
+        return bytes32(uint256(uint160(_addr)));
+    }
+}
---
+++ test/foundry/FraxOFTUpgradeable.t.sol
@@ -0,0 +1,5 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.20;
+
+// import "@fraxfinance/layerzero-v2-upgradeable/oapp/test/OFT.t.sol";
+

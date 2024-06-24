d5160bc44f4cf44eda502ea188115afd314b4520
Carter Carlson <pegahcarter@gmail.com>
2024-06-24 09:32:58 -0700

    refactor: BaseL0Script

---
+++ scripts/SubmitSends/SubmitSends.s.sol
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier: ISC
+pragma solidity ^0.8.19;
+
+import "../BaseL0Script.sol";
+
+contract SubmitSends is BaseL0Script {
+    function setUp() public override {
+        super.setUp();
+    }
+} 

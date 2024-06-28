0f0c90c1fbaad7b1a065d00540a18a8e3b4a437a
Carter Carlson <pegahcarter@gmail.com>
2024-06-28 16:05:07 -0700

    deploy(Mode): frxETH, FXS

---
+++ scripts/DeployFrxEthFpi/DeployFrxEthFpi.s.sol
@@ -0,0 +1,97 @@
+// SPDX-License-Identifier: ISC
+pragma solidity ^0.8.19;
+
+import "../DeployFraxOFTProtocol/DeployFraxOFTProtocol.s.sol";
+
+/*
+Intro:
+frxETH and FPI legacy OFTs were deployed after Mode and Sei were connected to 
+sfrxETH, sFRAX, FRAX, and FXS.
+
+Goal:
+Deploy frxETH and FPI upgradeable OFTs to Mode and Sei 
+*/
+
+contract DeployFrxEThFpis is DeployFraxOFTProtocol {
+    using OptionsBuilder for bytes;
+    using stdJson for string;
+    using Strings for uint256;
+
+    function version() public virtual override pure returns (uint256, uint256, uint256) {
+        return (1, 0, 0);
+    }
+
+    /// @dev override to alter file save location
+    modifier simulateAndWriteTxs(L0Config memory _config) override {
+        // Clear out arrays
+        delete enforcedOptionsParams;
+        delete setConfigParams;
+        delete serializedTxs;
+
+        vm.createSelectFork(_config.RPC);
+        chainid = _config.chainid;
+        vm.startPrank(_config.delegate);
+        _;
+        vm.stopPrank();
+
+        // create filename and save
+        string memory root = vm.projectRoot();
+        root = string.concat(root, "/scripts/DeployFrxEthFpi/txs/");
+        string memory filename = string.concat(activeConfig.chainid.toString(), "-");
+        filename = string.concat(filename, _config.chainid.toString());
+        filename = string.concat(filename, ".json");
+
+        new SafeTxUtil().writeTxs(serializedTxs, string.concat(root, filename));
+    }
+
+
+    function setUp() public override {
+        super.setUp();
+
+        // clear out legacyOfts
+        delete legacyOfts;
+        delete proxyOfts;
+
+        // set legacy OFTs with frxETH and FPI
+        legacyOfts.push(0xF010a7c8877043681D59AD125EbF575633505942);
+        legacyOfts.push(0xE41228a455700cAF09E551805A8aB37caa39D08c);
+    }
+
+    /// @dev no need for post-deploy checks
+    function deploySource() public virtual override {
+        preDeployChecks();
+        deployFraxOFTUpgradeablesAndProxies();
+        // postDeployChecks();
+    }
+
+    function deployFraxOFTUpgradeablesAndProxies() broadcastAs(oftDeployerPK) public virtual override {
+        // Implementation and proxy admin have already been deployed
+        implementationMock = 0x8f1B9c1fd67136D525E14D96Efb3887a33f16250;
+        proxyAdmin = 0x223a681fc5c5522c85C96157c0efA18cd6c5405c;
+
+        // Deploy frxETH
+        deployFraxOFTUpgradeableAndProxy({
+            _name: "Frax Ether",
+            _symbol: "frxETH"
+        });
+
+        // deploy FPI
+        deployFraxOFTUpgradeableAndProxy({
+            _name: "Frax Price Index",
+            _symbol: "FPI"
+        });
+    }
+
+    /// @dev override as the proxyAdmin ownership is already properly set
+    function setPriviledgedRoles() public virtual override {
+        for (uint256 o=0; o<proxyOfts.length; o++) {
+            address proxyOft = proxyOfts[o];
+            FraxOFTUpgradeable(proxyOft).setDelegate(activeConfig.delegate);
+            Ownable(proxyOft).transferOwnership(activeConfig.delegate);
+        }
+
+        /// @dev transfer ownership of ProxyAdmin
+        // Ownable(proxyAdmin).transferOwnership(activeConfig.delegate);
+    }
+
+}

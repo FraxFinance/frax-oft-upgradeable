d5f8c40d49c7d5ffea5df0944a9ba9695903547c
Carter Carlson <pegahcarter@gmail.com>
2024-10-07 08:55:29 -0700

    chore: save changes

--- script/solanaConverter.ts
+++ script/solanaConverter.ts
@@ -1,8 +1,10 @@
 import {addressToBytes32} from "@layerzerolabs/lz-v2-utilities";
 import { utils } from "ethers";
 
+// phantom 4nHaq4EiQZVnDhPAnK1zZEd3qKJVwzBuyTdrbBvvvUS3
+// cb 5KYEyuA1cAdnZFj4i6zUjTEre4s7snacyXbkTmNqLjJs
 function main() {
-    let converted = addressToBytes32("5KYEyuA1cAdnZFj4i6zUjTEre4s7snacyXbkTmNqLjJs");
+    let converted = addressToBytes32("0xb0E1650A9760e0f383174af042091fc544b8356f");
     let asHex = utils.hexlify(converted);
     console.log(asHex);
 }
--- scripts/SubmitSends/SubmitSends.s.sol
+++ scripts/SubmitSends/SubmitSends.s.sol
@@ -29,13 +29,13 @@ contract SubmitSends is BaseL0Script {
     function submitSends(
         address[] memory _connectedOfts
     ) public {
-        for (uint256 c=0; c<configs.length; c++) {
+        for (uint256 c=0; c<allConfigs.length; c++) {
             // Do not send if the target chain == active chain
-            if (configs[c].chainid == activeConfig.chainid) {
+            if (allConfigs[c].chainid == activeConfig.chainid) {
                 continue;
             }
             for (uint256 o=0; o<_connectedOfts.length; o++) {
-                submitSend(configs[c], _connectedOfts[o]);
+                submitSend(allConfigs[c], _connectedOfts[o]);
             }
         }
     }
@@ -48,8 +48,19 @@ contract SubmitSends is BaseL0Script {
     ) public broadcastAs(senderDeployerPK) {
         uint256 amount = 1e14;
         address oftToken = IOFT(_connectedOft).token();
+
+        // TODO: remove after testing
+        // send over sfrxEth, sFrax, frxEth
+        // if (_connectedOft != 0x5Bff88cA1442c2496f7E475E9e7786383Bc070c0 && _connectedOft != 0x3Ec3849C33291a9eF4c5dB86De593EB4A37fDe45 && _connectedOft != 0x43eDD7f3831b08FE70B7555ddD373C8bF65a9050) return;
+        // send legacy fpi
+        if (_connectedOft != 0x90581eCa9469D8D7F5D3B60f4715027aDFCf7927) return;
+        // skip sending to solana
+        if (_connectedConfig.eid == 30168) return;
+        // only send to proxy eids
+        // if (_connectedConfig.eid != 30260 && _connectedConfig.eid != 30280 && _connectedConfig.eid != 30255 && _connectedConfig.eid !=  30274) return;
+
         require(
-            IERC20(oftToken).balanceOf(vm.addr(senderDeployerPK)) > amount * 6,
+            IERC20(oftToken).balanceOf(vm.addr(senderDeployerPK)) > allConfigs.length,
             "Not enough token balance"
         );
 
@@ -60,9 +71,15 @@ contract SubmitSends is BaseL0Script {
 
         // bytes memory options = OptionsBuilder.newOptions().addExecutorLzReceiveOption(300_000, 0);
         bytes memory options = OptionsBuilder.newOptions();
+        bytes32 to;
+        if (_connectedConfig.eid == 30168) {
+            to = 0x382cf2ab7863c0f8cdedff8ef6ad1b02f835ddc8aea86c8f0ed5fc6fa8f765b0; // phantom wallet 4nHaq4EiQZVnDhPAnK1zZEd3qKJVwzBuyTdrbBvvvUS3
+        } else {
+            to = addressToBytes32(vm.addr(senderDeployerPK));
+        }
         SendParam memory sendParam = SendParam({
                 dstEid: uint32(_connectedConfig.eid),
-                to: addressToBytes32(vm.addr(senderDeployerPK)),
+                to: to,
                 amountLD: amount,
                 minAmountLD: amount,
                 extraOptions: options,

62b0097129436e8069d8e8cfd378714ecbe989f7
Carter Carlson <pegahcarter@gmail.com>
2024-12-30 13:02:08 -0800

    ops: only setup frxUSD peers for fraxtal, ink

--- scripts/DeploySonic/1_DeploySonic.s.sol
+++ scripts/DeploySonic/1_DeploySonic.s.sol
@@ -26,4 +26,44 @@ contract DeploySonic is DeployFraxOFTProtocol {
 
         setPriviledgedRoles();
     }
+
+    // override fraxtal frxUSD, sfrxUSD with standalone lockboxes
+    function setEvmPeers(
+        address[] memory _connectedOfts,
+        address[] memory _peerOfts,
+        L0Config[] memory _configs
+    ) public override {
+        require(_connectedOfts.length == _peerOfts.length, "Must wire equal amount of source + dest addrs");
+        // For each OFT
+        for (uint256 o=0; o<_connectedOfts.length; o++) {
+            // Set the config per chain
+            for (uint256 c=0; c<_configs.length; c++) {
+                address peerOft = _peerOfts[o];
+                // for fraxtal, override oft address of frxUSD/sfrxUSD to the standalone lockboxes
+                if (_configs[c].chainid == 252) {
+                    if (_connectedOfts[o] == frxUsdOft) {
+                        // standalone frxUSD lockbox
+                        peerOft = 0x96A394058E2b84A89bac9667B19661Ed003cF5D4;
+                    } else if (_connectedOfts[o] == sfrxUsdOft) {
+                        // standalone sfrxUSD lockbox
+                        peerOft = 0x88Aa7854D3b2dAA5e37E7Ce73A1F39669623a361;
+                    }
+                } else {
+                    // For frxUSD and sfrxUSD, only additionally set up Ink
+                    if (_connectedOfts[o] == frxUsdOft || _connectedOfts[o] == sfrxUsdOft) {
+                        if (_configs[c].chainid != 57073) {
+                            continue;
+                        }
+                    }
+                }
+                setPeer({
+                    _config: _configs[c],
+                    _connectedOft: _connectedOfts[o],
+                    _peerOftAsBytes32: addressToBytes32(/*_peerOfts[o]*/ peerOft)
+                });
+            }
+        }
+    }
+
+
 }

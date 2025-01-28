74adc269d80010c850e25f96ee32f26e1f7647ec
Carter Carlson <pegahcarter@gmail.com>
2025-01-28 13:54:39 -0800

    feat: add setLibs to dest

--- scripts/DeployFraxOFTProtocol/DeployFraxOFTProtocol.s.sol
+++ scripts/DeployFraxOFTProtocol/DeployFraxOFTProtocol.s.sol
@@ -15,7 +15,7 @@ contract DeployFraxOFTProtocol is BaseL0Script {
     using Strings for uint256;
 
     function version() public virtual override pure returns (uint256, uint256, uint256) {
-        return (1, 2, 6);
+        return (1, 2, 7);
     }
 
     function setUp() public virtual override {
@@ -68,6 +68,12 @@ contract DeployFraxOFTProtocol is BaseL0Script {
         });
 
         setDVNs({
+            _connectedConfig: _connectedConfig,
+            _connectedOfts: proxyOfts,
+            _configs: broadcastConfigArray
+        });
+
+        setLibs({
             _connectedConfig: _connectedConfig,
             _connectedOfts: proxyOfts,
             _configs: broadcastConfigArray

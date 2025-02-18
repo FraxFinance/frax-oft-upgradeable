03de71335f462258f40df6d91a2f93a13f3018b7
dhruvinparikh <imsilent93@gmail.com>
2025-02-18 18:54:22 +0000

    (chore): add OFT init params

---
+++ deploy-move/FPIInitParams.ts
@@ -0,0 +1,10 @@
+const oftMetadata = {
+    token_name: 'Frax Price Index',
+    token_symbol: 'FPI',
+    icon_uri: '',
+    project_uri: '',
+    sharedDecimals: 6,
+    localDecimals: 6,
+}
+
+export { oftMetadata }

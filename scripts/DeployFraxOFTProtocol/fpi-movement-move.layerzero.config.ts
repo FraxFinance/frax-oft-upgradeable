a46c81b4b9020b998c452b7285ede97acafd322a
dhruvinparikh <imsilent93@gmail.com>
2025-03-20 19:31:15 +0000

    (ref): fpi config fix on movement

--- scripts/DeployFraxOFTProtocol/fpi-movement-move.layerzero.config.ts
+++ scripts/DeployFraxOFTProtocol/fpi-movement-move.layerzero.config.ts
@@ -1,17 +1,12 @@
 import { EndpointId } from '@layerzerolabs/lz-definitions'
-import { ExecutorOptionType } from '@layerzerolabs/lz-v2-utilities'
 
 import type { OAppOmniGraphHardhat, OmniPointHardhat } from '@layerzerolabs/toolbox-hardhat'
-
-enum MsgType {
-    SEND = 1,
-    SEND_AND_CALL = 2,
-}
+import { connectionConfig } from './l0-movement-connection-config'
 
 const fraxtalContract: OmniPointHardhat = {
     eid: EndpointId.FRAXTAL_V2_MAINNET,
     address: "0x75c38D46001b0F8108c4136216bd2694982C20FC",
-    contractName:"FraxOFTAdapterUpgradeable"
+    contractName: "FraxOFTAdapterUpgradeable"
 }
 
 const movementContract: OmniPointHardhat = {
@@ -40,117 +35,8 @@ const config: OAppOmniGraphHardhat = {
         {
             from: movementContract,
             to: fraxtalContract,
-            config: {
-                enforcedOptions: [
-                    {
-                        msgType: MsgType.SEND,
-                        optionType: ExecutorOptionType.LZ_RECEIVE,
-                        gas: 80_000, // gas limit in wei for EndpointV2.lzReceive
-                        value: 0, // msg.value in wei for EndpointV2.lzReceive
-                    },
-                    {
-                        msgType: MsgType.SEND_AND_CALL,
-                        optionType: ExecutorOptionType.LZ_RECEIVE,
-                        gas: 80_000, // gas limit in wei for EndpointV2.lzReceive
-                        value: 0, // msg.value in wei for EndpointV2.lzReceive
-                    },
-                ],
-                sendLibrary: '0xc33752e0220faf79e45385dd73fb28d681dcd9f1569a1480725507c1f3c3aba9',
-                receiveLibraryConfig: {
-                    // Required Receive Library Address on Movement
-                    receiveLibrary: '0xc33752e0220faf79e45385dd73fb28d681dcd9f1569a1480725507c1f3c3aba9',
-                    // Optional Grace Period for Switching Receive Library Address on Movement
-                    gracePeriod: BigInt(0),
-                },
-                // Optional Receive Library Timeout for when the Old Receive Library Address will no longer be valid on Movement
-                // receiveLibraryTimeoutConfig: {
-                //     lib: '',
-                //     expiry: BigInt(1000000000),
-                // },
-                sendConfig: {
-                    executorConfig: {
-                        maxMessageSize: 10_000,
-                        // The configured Executor address on Movement
-                        executor: '0x15a5bbf1eb7998a22c9f23810d424abe40bd59ddd8e6ab7e59529853ebed41c4',
-                    },
-                    ulnConfig: {
-                        // The number of block confirmations to wait on Aptos before emitting the message from the source chain.
-                        confirmations: BigInt(1000000),
-                        // The address of the DVNs you will pay to verify a sent message on the source chain.
-                        // The destination tx will wait until ALL `requiredDVNs` verify the message.
-                        requiredDVNs: ['0x2b696b3ee859b7eb624e1fd5de49f4d3806f49862f1177d6827fd1beffde9179','0xdf8f0a53b20f1656f998504b81259698d126523a31bdbbae45ba1e8a3078d8da'],
-                        // The address of the DVNs you will pay to verify a sent message on the source chain.
-                        // The destination tx will wait until the configured threshold of `optionalDVNs` verify a message.
-                        optionalDVNs: [],
-                        // The number of `optionalDVNs` that need to successfully verify the message for it to be considered Verified.
-                        optionalDVNThreshold: 0,
-                    },
-                },
-                // Optional Receive Configuration
-                // @dev Controls how the `from` chain receives messages from the `to` chain.
-                receiveConfig: {
-                    ulnConfig: {
-                        // The number of block confirmations to expect from the `to` chain.
-                        confirmations: BigInt(1000000),
-                        // The address of the DVNs your `receiveConfig` expects to receive verifications from on the `from` chain.
-                        // The `from` chain's OApp will wait until the configured threshold of `requiredDVNs` verify the message.
-                        requiredDVNs: ['0x2b696b3ee859b7eb624e1fd5de49f4d3806f49862f1177d6827fd1beffde9179','0xdf8f0a53b20f1656f998504b81259698d126523a31bdbbae45ba1e8a3078d8da'],
-                        // The address of the `optionalDVNs` you expect to receive verifications from on the `from` chain.
-                        // The destination tx will wait until the configured threshold of `optionalDVNs` verify the message.
-                        optionalDVNs: [],
-                        // The number of `optionalDVNs` that need to successfully verify the message for it to be considered Verified.
-                        optionalDVNThreshold: 0,
-                    },
-                },
-            },
-        },
-        {
-            from: fraxtalContract,
-            to: movementContract,
-            config: {
-                enforcedOptions: [
-                    {
-                        msgType: MsgType.SEND,
-                        optionType: ExecutorOptionType.LZ_RECEIVE,
-                        gas: 5_000, // gas limit in wei for EndpointV2.lzReceive
-                        value: 0, // msg.value in wei for EndpointV2.lzReceive
-                    },
-                    {
-                        msgType: MsgType.SEND_AND_CALL,
-                        optionType: ExecutorOptionType.LZ_RECEIVE,
-                        gas: 5_000, // gas limit in wei for EndpointV2.lzCompose
-                        value: 0, // msg.value in wei for EndpointV2.lzCompose
-                    },
-                ],
-                sendLibrary: '0x377530cdA84DFb2673bF4d145DCF0C4D7fdcB5b6',
-                receiveLibraryConfig: {
-                    receiveLibrary: '0x8bC1e36F015b9902B54b1387A4d733cebc2f5A4e',
-                    gracePeriod: BigInt(0),
-                },
-                // receiveLibraryTimeoutConfig: {
-                //     lib: '0x188d4bbCeD671A7aA2b5055937F79510A32e9683',
-                //     expiry: BigInt(67323472),
-                // },
-                sendConfig: {
-                    executorConfig: {
-                        maxMessageSize: 10_000,
-                        executor: '0x377530cdA84DFb2673bF4d145DCF0C4D7fdcB5b6',
-                    },
-                    ulnConfig: {
-                        confirmations: BigInt(5),
-                        requiredDVNs: ['0xcce466a522984415bc91338c232d98869193d46e','0xdd7b5e1db4aafd5c8ec3b764efb8ed265aa5445b'],
-                        optionalDVNThreshold: 0,
-                    },
-                },
-                receiveConfig: {
-                    ulnConfig: {
-                        confirmations: BigInt(5),
-                        requiredDVNs: ['0xcce466a522984415bc91338c232d98869193d46e','0xdd7b5e1db4aafd5c8ec3b764efb8ed265aa5445b'],
-                        optionalDVNThreshold: 0,
-                    },
-                },
-            },
-        },
+            config: connectionConfig,
+        }
     ],
 }
 

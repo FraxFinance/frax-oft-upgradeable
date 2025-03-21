d0054e3046e9436a2241e4f28b5fdfb9dc7f113a
dhruvinparikh <imsilent93@gmail.com>
2025-03-21 18:48:06 +0000

    (chore): sfrxusd oft on aptos

--- scripts/DeployFraxOFTProtocol/sfrxusd-aptos-move.layerzero.config.ts
+++ scripts/DeployFraxOFTProtocol/sfrxusd-aptos-move.layerzero.config.ts
@@ -1,15 +1,37 @@
 import { EndpointId } from '@layerzerolabs/lz-definitions'
 
-import type { OmniPointHardhat } from '@layerzerolabs/toolbox-hardhat'
+import type { OAppOmniGraphHardhat, OmniPointHardhat } from '@layerzerolabs/toolbox-hardhat'
+import { aptosContractConfig, aptosToFraxtalconnectionConfig, fraxtalContractConfig } from './l0-move-connection-config'
 
-import { GenerateConfig } from './generate-L0-move-config';
-
+const fraxtalContract: OmniPointHardhat = {
+    eid: EndpointId.FRAXTAL_V2_MAINNET,
+    address: "0x88Aa7854D3b2dAA5e37E7Ce73A1F39669623a361",
+    contractName: "FraxOFTAdapterUpgradeable"
+}
 
 const aptosContract: OmniPointHardhat = {
     eid: EndpointId.APTOS_V2_MAINNET,
     contractName: 'sfrxUSD',
 }
 
-const config = GenerateConfig(aptosContract);
+const config: OAppOmniGraphHardhat = {
+    contracts: [
+        {
+            contract: fraxtalContract,
+            config: fraxtalContractConfig,
+        },
+        {
+            contract: aptosContract,
+            config: aptosContractConfig,
+        },
+    ],
+    connections: [
+        {
+            from: aptosContract,
+            to: fraxtalContract,
+            config: aptosToFraxtalconnectionConfig,
+        },
+    ],
+}
 
 export default config

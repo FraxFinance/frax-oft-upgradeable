90dbbe7118859e7d9826ecaa16456fbced1144f6
dhruvinparikh <imsilent93@gmail.com>
2025-03-21 17:53:00 +0000

    (chore): fpi oft on aptos

--- scripts/DeployFraxOFTProtocol/fpi-aptos-move.layerzero.config.ts
+++ scripts/DeployFraxOFTProtocol/fpi-aptos-move.layerzero.config.ts
@@ -1,15 +1,37 @@
 import { EndpointId } from '@layerzerolabs/lz-definitions'
 
-import type { OmniPointHardhat } from '@layerzerolabs/toolbox-hardhat'
+import type { OAppOmniGraphHardhat, OmniPointHardhat } from '@layerzerolabs/toolbox-hardhat'
+import { fraxtalContractConfig, aptosContractConfig, aptosToFraxtalconnectionConfig } from './l0-move-connection-config'
 
-import { GenerateConfig } from './generate-L0-move-config';
+const fraxtalContract: OmniPointHardhat = {
+    eid: EndpointId.FRAXTAL_V2_MAINNET,
+    address: "0x75c38D46001b0F8108c4136216bd2694982C20FC",
+    contractName: "FraxOFTAdapterUpgradeable"
+}
 
-
-const contract: OmniPointHardhat = {
+const aptosContract: OmniPointHardhat = {
     eid: EndpointId.APTOS_V2_MAINNET,
     contractName: 'FPI',
 }
 
-const config = GenerateConfig(contract);
+const config: OAppOmniGraphHardhat = {
+    contracts: [
+        {
+            contract: fraxtalContract,
+            config: fraxtalContractConfig,
+        },
+        {
+            contract: aptosContract,
+            config: aptosContractConfig,
+        },
+    ],
+    connections: [
+        {
+            from: aptosContract,
+            to: fraxtalContract,
+            config: aptosToFraxtalconnectionConfig,
+        }
+    ],
+}
 
 export default config
--- scripts/DeployFraxOFTProtocol/l0-move-connection-config.ts
+++ scripts/DeployFraxOFTProtocol/l0-move-connection-config.ts
@@ -71,12 +71,82 @@ export const movementToFraxtalconnectionConfig: OAppEdgeConfig = {
     },
 }
 
+export const aptosToFraxtalconnectionConfig: OAppEdgeConfig = {
+
+    enforcedOptions: [
+        {
+            msgType: MsgType.SEND,
+            optionType: ExecutorOptionType.LZ_RECEIVE,
+            gas: 80_000, // gas limit in wei for EndpointV2.lzReceive
+            value: 0, // msg.value in wei for EndpointV2.lzReceive
+        },
+        {
+            msgType: MsgType.SEND_AND_CALL,
+            optionType: ExecutorOptionType.LZ_RECEIVE,
+            gas: 80_000, // gas limit in wei for EndpointV2.lzReceive
+            value: 0, // msg.value in wei for EndpointV2.lzReceive
+        },
+    ],
+    sendLibrary: '0xc33752e0220faf79e45385dd73fb28d681dcd9f1569a1480725507c1f3c3aba9',
+    receiveLibraryConfig: {
+        // Required Receive Library Address on Movement
+        receiveLibrary: '0xc33752e0220faf79e45385dd73fb28d681dcd9f1569a1480725507c1f3c3aba9',
+        // Optional Grace Period for Switching Receive Library Address on Movement
+        gracePeriod: BigInt(0),
+    },
+    // Optional Receive Library Timeout for when the Old Receive Library Address will no longer be valid on Movement
+    // receiveLibraryTimeoutConfig: {
+    //     lib: '',
+    //     expiry: BigInt(1000000000),
+    // },
+    sendConfig: {
+        executorConfig: {
+            maxMessageSize: 10_000,
+            // The configured Executor address on Movement
+            executor: '0x15a5bbf1eb7998a22c9f23810d424abe40bd59ddd8e6ab7e59529853ebed41c4',
+        },
+        ulnConfig: {
+            // The number of block confirmations to wait on Aptos before emitting the message from the source chain.
+            confirmations: BigInt(1000000),
+            // The address of the DVNs you will pay to verify a sent message on the source chain.
+            // The destination tx will wait until ALL `requiredDVNs` verify the message.
+            requiredDVNs: ['0x93adea241d46ddebc207d74402ff9a150f70b9de828b8b2208d69b7d08e90bd7', '0xdf8f0a53b20f1656f998504b81259698d126523a31bdbbae45ba1e8a3078d8da'],
+            // The address of the DVNs you will pay to verify a sent message on the source chain.
+            // The destination tx will wait until the configured threshold of `optionalDVNs` verify a message.
+            optionalDVNs: [],
+            // The number of `optionalDVNs` that need to successfully verify the message for it to be considered Verified.
+            optionalDVNThreshold: 0,
+        },
+    },
+    // Optional Receive Configuration
+    // @dev Controls how the `from` chain receives messages from the `to` chain.
+    receiveConfig: {
+        ulnConfig: {
+            // The number of block confirmations to expect from the `to` chain.
+            confirmations: BigInt(5),
+            // The address of the DVNs your `receiveConfig` expects to receive verifications from on the `from` chain.
+            // The `from` chain's OApp will wait until the configured threshold of `requiredDVNs` verify the message.
+            requiredDVNs: ['0x93adea241d46ddebc207d74402ff9a150f70b9de828b8b2208d69b7d08e90bd7', '0xdf8f0a53b20f1656f998504b81259698d126523a31bdbbae45ba1e8a3078d8da'],
+            // The address of the `optionalDVNs` you expect to receive verifications from on the `from` chain.
+            // The destination tx will wait until the configured threshold of `optionalDVNs` verify the message.
+            optionalDVNs: [],
+            // The number of `optionalDVNs` that need to successfully verify the message for it to be considered Verified.
+            optionalDVNThreshold: 0,
+        },
+    },
+}
+
 export const fraxtalContractConfig = {
     owner: '0x5f25218ed9474b721d6a38c115107428E832fA2E',
     delegate: '0x5f25218ed9474b721d6a38c115107428E832fA2E',
 }
 
 export const movementContractConfig = {
+    delegate: '09d0eb2763c96e085fa74ba6cf0d49136f8654c48ec7dbc59279a9066c7dd409',
+    owner: '09d0eb2763c96e085fa74ba6cf0d49136f8654c48ec7dbc59279a9066c7dd409',
+}
+
+export const aptosContractConfig = {
     delegate: '09d0eb2763c96e085fa74ba6cf0d49136f8654c48ec7dbc59279a9066c7dd409',
     owner: '09d0eb2763c96e085fa74ba6cf0d49136f8654c48ec7dbc59279a9066c7dd409',
 }

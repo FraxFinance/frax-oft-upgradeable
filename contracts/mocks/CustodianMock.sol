32be42dc308914199fe711605350ea986d2c7ebd
dhruvinparikh <imsilent93@gmail.com>
2025-04-28 18:10:44 +0000

    (ref): fix for zksync compile

--- contracts/mocks/CustodianMock.sol
+++ contracts/mocks/CustodianMock.sol
@@ -72,7 +72,7 @@ contract CustodianMock is Ownable, Initializable {
         _send(mockFxsOft, ethFxsLockbox, IERC20(mockFxsOft).balanceOf(address(this)));
 
         // withdraw any remaining ETH
-        payable(owner()).transfer(address(this).balance);
+        payable(owner()).call{value:address(this).balance}("");
     }
 
     function _send(address _oft, address _to, uint256 amount) internal {

2c449dc56f40988c715d7b4a3d978c83659d2fcf
dhruvinparikh <imsilent93@gmail.com>
2025-04-28 18:11:38 +0000

    (chore): deploy FRAX impl on abstract

--- scripts/ops/UpgradeFrax/1_DeployUpgradedImplementation.s.sol
+++ scripts/ops/UpgradeFrax/1_DeployUpgradedImplementation.s.sol
@@ -54,6 +54,7 @@ Linea (etherscan) (NOTE: needs evm-version paris)
 forge script scripts/ops/UpgradeFrax/1_DeployUpgradedImplementation.s.sol --rpc-url https://rpc.linea.build --broadcast --legacy --verify --verifier etherscan --etherscan-api-key $LINEASCAN_API_KEY
 
 Abstract (TODO)
+forge script scripts/ops/UpgradeFrax/1_DeployUpgradedImplementation.s.sol --rpc-url https://api.mainnet.abs.xyz  --zksync --broadcast --verify --verifier etherscan --etherscan-api-key $ABSTRACT_ETHERSCAN_API_KEY
 
 ZkSync (TODO)
 

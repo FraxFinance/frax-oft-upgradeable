cc9feb9bd3b6d6772af22b278baba8eeda20cc16
Dhruvin Parikh <29928916+dhruvinparikh@users.noreply.github.com>
2025-05-12 12:35:09 -0400

    Rename FXS -> FRAX on Solana OFT (#74)
    * (chore): solana frxeth enforced options
    
    * (chore): solana frxusd enforced options
    
    * (chore): solana fxs enforced options
    
    * (chore): solana sfrxeth enforced options
    
    * (chore): solana sfrxusd enforced options
    
    * (ref): solna->evm gas limit to 200k
    
    * (feat): init adding multi sig support to update metadata
    
    * (chore): upgrade LZ deps for solana OFT update
    
    * (ref): L0 solana as per new config
    
    * (feat): init multisig capability
    
    * (feat): add update metadata squad task
    
    * (ref): frax to wrapped frax on solana
    
    ---------
    
    Co-authored-by: Carter Carlson <pegahcarter@gmail.com>

--- tasks/solana/updateMetaDataSquad.ts
+++ tasks/solana/updateMetaDataSquad.ts
@@ -4,14 +4,17 @@ import {
     fetchMetadataFromSeeds,
     updateV1,
 } from '@metaplex-foundation/mpl-token-metadata'
-import { publicKey, transactionBuilder } from '@metaplex-foundation/umi'
+import { createNoopSigner, publicKey, signerIdentity, transactionBuilder } from '@metaplex-foundation/umi'
+import { createUmi } from '@metaplex-foundation/umi-bundle-defaults'
 import bs58 from 'bs58'
 import { task } from 'hardhat/config'
 
 import { types as devtoolsTypes } from '@layerzerolabs/devtools-evm-hardhat'
 import { EndpointId } from '@layerzerolabs/lz-definitions'
 
-import { deriveConnection, getExplorerTxLink } from './index'
+import { VersionedMessage, VersionedTransaction } from '@solana/web3.js'
+import { mplToolbox } from '@metaplex-foundation/mpl-toolbox'
+import { createSolanaConnectionFactory } from '../common/utils'
 
 interface UpdateMetadataTaskArgs {
     eid: EndpointId
@@ -20,26 +23,31 @@ interface UpdateMetadataTaskArgs {
     sellerFeeBasisPoints: number
     symbol: string
     uri: string
+    squadsAuthority: string
 }
 
 // note that if URI is specified, then the name and symbol in there would be used and will override the 'outer' name and symbol
-task('lz:oft:solana:update-metadata', 'Updates the metaplex metadata of the SPL Token')
+task('lz:oft:solana:update-metadata:squads', 'Updates the metaplex metadata of the SPL Token')
     .addParam('eid', 'Solana mainnet (30168) or testnet (40168)', undefined, devtoolsTypes.eid)
     .addParam('mint', 'The Token mint public key', undefined, devtoolsTypes.string)
+    .addParam('squadsAuthority', 'The Squads authority public key', undefined, devtoolsTypes.string)
     .addOptionalParam('name', 'Token Name', undefined, devtoolsTypes.string)
     .addOptionalParam('symbol', 'Token Symbol', undefined, devtoolsTypes.string)
     .addOptionalParam('sellerFeeBasisPoints', 'Seller fee basis points', undefined, devtoolsTypes.int)
     .addOptionalParam('uri', 'URI for token metadata', undefined, devtoolsTypes.string)
-    .setAction(async ({ eid, name, mint: mintStr, sellerFeeBasisPoints, symbol, uri }: UpdateMetadataTaskArgs) => {
-        const { umi, umiWalletSigner } = await deriveConnection(eid)
+    .setAction(async ({ eid, name, mint: mintStr, squadsAuthority: squadsAuthorityStr, sellerFeeBasisPoints, symbol, uri }: UpdateMetadataTaskArgs) => {
+        const connectionFactory = createSolanaConnectionFactory()
+        const connection = await connectionFactory(eid)
+        const umi = createUmi(connection.rpcEndpoint).use(mplToolbox())
+
+        let squadsAuthority = publicKey(squadsAuthorityStr);
+        const squadsSigner = createNoopSigner(squadsAuthority);
+
+        umi.use(signerIdentity(squadsSigner))
 
         const mint = publicKey(mintStr)
 
         const initialMetadata = await fetchMetadataFromSeeds(umi, { mint })
-
-        if (initialMetadata.updateAuthority !== umiWalletSigner.publicKey.toString()) {
-            throw new Error('Only the update authority can update the metadata')
-        }
 
         if (initialMetadata.isMutable == false) {
             throw new Error('Metadata is not mutable')
@@ -49,7 +57,7 @@ task('lz:oft:solana:update-metadata', 'Updates the metaplex metadata of the SPL 
 
         const updateV1Args: UpdateV1InstructionAccounts & UpdateV1InstructionArgs = {
             mint,
-            authority: umiWalletSigner,
+            authority: squadsSigner,
             data: {
                 ...initialMetadata,
                 name: name || initialMetadata.name,
@@ -61,6 +69,16 @@ task('lz:oft:solana:update-metadata', 'Updates the metaplex metadata of the SPL 
         }
 
         const txBuilder = transactionBuilder().add(updateV1(umi, updateV1Args))
-        const createTokenTx = await txBuilder.sendAndConfirm(umi)
-        console.log(`createTokenTx: ${getExplorerTxLink(bs58.encode(createTokenTx.signature), isTestnet)}`)
+        txBuilder.setFeePayer(squadsSigner)
+        await txBuilder.setLatestBlockhash(umi)
+        const serializedTx = await txBuilder.buildWithLatestBlockhash(umi)
+        const transactionDataHex = Buffer.from(serializedTx.serializedMessage).toString("hex")
+        const versionedMessage = VersionedMessage.deserialize(Buffer.from(transactionDataHex, 'hex'))
+
+        const tx = new VersionedTransaction(versionedMessage);
+
+        console.log('BASE58: \n')
+        console.log(bs58.encode(Buffer.from(tx.serialize())))
+        console.log('\nBASE64: \n')
+        console.log(Buffer.from(tx.serialize()).toString("base64"));
     })

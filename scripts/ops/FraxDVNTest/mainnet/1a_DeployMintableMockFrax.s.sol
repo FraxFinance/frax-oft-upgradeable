0d1f32a63b003e926f8155b4143a5d995b0f30da
dhruvinparikh <imsilent93@gmail.com>
2025-06-16 05:05:36 +0000

    (ref): cosmetic changes L0Config and TODOs

--- scripts/ops/FraxDVNTest/mainnet/1a_DeployMintableMockFrax.s.sol
+++ scripts/ops/FraxDVNTest/mainnet/1a_DeployMintableMockFrax.s.sol
@@ -5,6 +5,13 @@ import "scripts/DeployFraxOFTProtocol/DeployFraxOFTProtocol.s.sol";
 import "contracts/FraxOFTMintableUpgradeable.sol";
 import { FraxOFTWalletUpgradeable } from "contracts/FraxOFTWalletUpgradeable.sol";
 
+// fraxtal : forge script ./scripts/ops/FraxDVNTest/mainnet/1a_DeployMintableMockFrax.s.sol --rpc-url https://rpc.frax.com --verifier-url $FRAXSCAN_API_URL --etherscan-api-key $FRAXSCAN_API_KEY --verify --broadcast
+// verify Implementation Mock : forge verify-contract 0x01c9C9aD9FCF0Be1Bb028A503DA27BaE1eEe8205 ./contracts/mocks/ImplementationMock.sol:ImplementationMock --chain-id 252 --watch --verifier etherscan --etherscan-api-key $FRAXSCAN_API_KEY --verifier-url $FRAXSCAN_API_URL
+// verify FraxOFTMintableUpgradeable : forge verify-contract 0x7130176a1f87e29d2c4543f3bd3edc245ea6b70d ./contracts/FraxOFTMintableUpgradeable.sol:FraxOFTMintableUpgradeable --constructor-args $(cast abi-encode "constructor(address)" 0x1a44076050125825900e736c501f859c50fE728c)  --chain-id 252 --watch --verifier etherscan --etherscan-api-key $FRAXSCAN_API_KEY --verifier-url $FRAXSCAN_API_URL
+// verify TransparentUpgradeableProxy : forge verify-contract 0x57558Cb8d6005DE0BAe8a2789d5EfaaE52dba5a8 ./node_modules/@fraxfinance/layerzero-v2-upgradeable/messagelib/contracts/upgradeable/proxy/TransparentUpgradeableProxy.sol:TransparentUpgradeableProxy --constructor-args $(cast abi-encode "constructor(address,address,bytes)" 0x01c9C9aD9FCF0Be1Bb028A503DA27BaE1eEe8205 0x45DCE8e4F2Dc005A5F28206A46cb034697eEDA8e 0x)  --chain-id 252 --watch --verifier etherscan --etherscan-api-key $FRAXSCAN_API_KEY --verifier-url $FRAXSCAN_API_URL
+// verify FraxOFTWalletUpgradeable : forge verify-contract 0x5b9d960752963c92317b76adacfcc104f69f31a7 ./contracts/FraxOFTWalletUpgradeable.sol:FraxOFTWalletUpgradeable  --chain-id 252 --watch --verifier etherscan --etherscan-api-key $FRAXSCAN_API_KEY --verifier-url $FRAXSCAN_API_URL
+// verify TransparentUpgradeableProxy : forge verify-contract 0x741F0d8Bde14140f62107FC60A0EE122B37D4630 ./node_modules/@fraxfinance/layerzero-v2-upgradeable/messagelib/contracts/upgradeable/proxy/TransparentUpgradeableProxy.sol:TransparentUpgradeableProxy --constructor-args $(cast abi-encode "constructor(address,address,bytes)" 0x01c9C9aD9FCF0Be1Bb028A503DA27BaE1eEe8205 0x45DCE8e4F2Dc005A5F28206A46cb034697eEDA8e 0x)  --chain-id 252 --watch --verifier etherscan --etherscan-api-key $FRAXSCAN_API_KEY --verifier-url $FRAXSCAN_API_URL
+
 contract DeployMintableMockFrax is DeployFraxOFTProtocol {
     address[] public proxyOftWallets;
 

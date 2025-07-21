b92699e816e6bab68882756886d49d85269d213f
Carter Carlson <pegahcarter@gmail.com>
2025-07-21 13:29:06 -0700

    fix: set dvns and libs

--- scripts/ops/fix/FixEthSolanaWFraxWire/FixEthSolanaWFraxWire.s.sol
+++ scripts/ops/fix/FixEthSolanaWFraxWire/FixEthSolanaWFraxWire.s.sol
@@ -9,6 +9,7 @@ contract FixEthSolanaWFraxWire is DeployFraxOFTProtocol {
     using stdJson for string;
     using Strings for uint256;
 
+
     function filename() public view override returns (string memory) {
         string memory root = vm.projectRoot();
         root = string.concat(root, "/scripts/ops/fix/FixEthSolanaWFraxWire/");
@@ -17,25 +18,39 @@ contract FixEthSolanaWFraxWire is DeployFraxOFTProtocol {
     }
 
     function run() public override {
-        // double check that we're dealing with solana config
-        require(nonEvmConfigs[0].eid == 30168, "Not Solana config");
-
-        wireWFrax();
+        setupSource();
     }
 
-    function wireWFrax() public simulateAndWriteTxs(broadcastConfig) {
+    function setupSource() public override simulateAndWriteTxs(broadcastConfig) {
         connectedOfts = new address[](1);
         connectedOfts[0] = ethFraxOft;
 
-        /// @dev these functions are derived from DeployFraxOFTProtocol.setupNonEvms().
+        // double check that we're dealing with solana config
+        require(nonEvmConfigs[0].eid == 30168, "Not Solana config");
+        L0Config[] memory solanaConfigAsArray = new L0Config[](1);
+        solanaConfigAsArray[0] = nonEvmConfigs[0];
+
+        /// @dev derived from DeployFraxOFTProtocol.setupNonEvms().
         setSolanaEnforcedOptions({
             _connectedOfts: connectedOfts
         });
-
         setPeer({
             _config: nonEvmConfigs[0],
             _connectedOft: ethFraxOft,
             _peerOftAsBytes32: 0x4939035f8dd13d15a9386e28b6705519aa6f488791323466a3c0116a201e51aa // from NonEvmPeers.json
         });
+
+        /// @dev derived from DeployFraxOFTProtocol.setupSource().
+        setDVNs({
+            _connectedConfig: broadcastConfig,
+            _connectedOfts: connectedOfts,
+            _configs: solanaConfigAsArray
+        });
+        setLibs({
+            _connectedConfig: broadcastConfig,
+            _connectedOfts: connectedOfts,
+            _configs: solanaConfigAsArray
+        });
+
     }
 }

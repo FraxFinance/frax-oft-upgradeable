97886d9de00b8751868eb3269ae5354e82ec7fc0
dhruvinparikh <imsilent93@gmail.com>
2025-09-13 19:47:22 +0000

    (chore): setup mockfrax on hyperliquid

--- scripts/ops/FraxDVNTest/mainnet/3_SetupMockFrax.s.sol
+++ scripts/ops/FraxDVNTest/mainnet/3_SetupMockFrax.s.sol
@@ -37,7 +37,7 @@ contract SetupMockFrax is DeployFraxOFTProtocol {
             expectedProxyOfts.push(0xA057D8D4Fc86a62801CE363C169b0A8d192F6cEE);
         } else {
             // 1,81457,8453,34443,1329,252,196,146,57073,42161,10,137,43114,56,1101,80094,480,130,98866,747474,
-            // 534352
+            // 534352, 999
             connectedOfts[0] = 0x57558Cb8d6005DE0BAe8a2789d5EfaaE52dba5a8;
             proxyOfts.push(0x57558Cb8d6005DE0BAe8a2789d5EfaaE52dba5a8);
             expectedProxyOfts.push(0x57558Cb8d6005DE0BAe8a2789d5EfaaE52dba5a8);
@@ -71,7 +71,7 @@ contract SetupMockFrax is DeployFraxOFTProtocol {
             peer = 0xA057D8D4Fc86a62801CE363C169b0A8d192F6cEE;
         } else {
             // 1,81457,8453,34443,1329,252,196,146,57073,42161,10,137,43114,56,1101,80094,480,130,98866,747474,
-            // 534352
+            // 534352, 999
             peer = 0x57558Cb8d6005DE0BAe8a2789d5EfaaE52dba5a8;
         }
     }
@@ -82,7 +82,7 @@ contract SetupMockFrax is DeployFraxOFTProtocol {
         if (_config.eid == 30168) return;
         if (_config.eid == 30151) return;
 
-        if (_config.eid != 30211 && _config.eid != 30214) return; // change the eid to desired remote
+        // if (_config.eid != 30367) return; // Note. change the eid to desired remote
 
         if (_connectedConfig.eid == 30370) {
             // if broadcast config is plumephoenix,
@@ -109,6 +109,12 @@ contract SetupMockFrax is DeployFraxOFTProtocol {
             // aptos (30108)
             if (_config.eid == 30108) return;
         }
+
+        if (_connectedConfig.eid == 30367) {
+            // L0 team has not setup defaultSendLibrary and defaultReceiveLibrary on hyperliquid for
+            // Botanix (botanixlabs)
+            if (_config.eid == 30376) return;
+        }
         // TODO : only set library if it is not same as config
         super.setLib(_connectedConfig, _connectedOft, _config);
     }
@@ -122,7 +128,7 @@ contract SetupMockFrax is DeployFraxOFTProtocol {
         if (_dstConfig.eid == 30168) return;
         if (_dstConfig.eid == 30151) return;
 
-        if (_dstConfig.eid != 30211 && _dstConfig.eid != 30214) return; // change the eid to desired remote
+        // if (_dstConfig.eid != 30367) return; // Note. change the eid to desired remote
 
         if (_srcConfig.eid == 30370) {
             // L0 team has not setup defaultSendLibrary and defaultReceiveLibrary on plumephoenix for
@@ -148,6 +154,11 @@ contract SetupMockFrax is DeployFraxOFTProtocol {
             // L0 team has not setup defaultSendLibrary and defaultReceiveLibrary on aurora for
             // Aptos (30108)
             if (_dstConfig.eid == 30108) return;
+        }
+        if (_srcConfig.eid == 30367) {
+            // L0 team has not setup defaultSendLibrary and defaultReceiveLibrary on hyperliquid for
+            // Botanix (botanixlabs)
+            if (_dstConfig.eid == 30376) return;
         }
         super.setConfig(_srcConfig, _dstConfig, _lib, _oft);
     }

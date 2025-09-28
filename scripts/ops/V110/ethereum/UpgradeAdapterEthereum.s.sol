d4cecdd7779b79a4104867957d8c7fd6e1b2c1dc
Carter Carlson <pegahcarter@gmail.com>
2025-09-28 01:06:28 -0700

    fix: 3.6

--- scripts/ops/V110/ethereum/UpgradeAdapterEthereum.s.sol
+++ scripts/ops/V110/ethereum/UpgradeAdapterEthereum.s.sol
@@ -10,45 +10,52 @@ interface IERC20PermitPermissionedOptiMintable {
 
 // forge script scripts/ops/V110/ethereum/UpgradeAdapterEthereum.s.sol --rpc-url https://eth-mainnet.public.blastapi.io
 contract UpgradeAdapter is DeployFraxOFTProtocol {
-    
+    using Strings for uint256;
+    using Strings for address;
+
     address frxUsd = 0xCAcd6fd266aF91b8AeD52aCCc382b4e165586E29;
     address sfrxUsd = 0xcf62F905562626CfcDD2261162a51fd02Fc9c5b6;
 
     address frxUsdMintableLockboxImp;
     address sfrxUsdMintableLockboxImp;
 
+    uint256 filecount;
+
     modifier prankAndWriteTxs(address who) {
         vm.startPrank(who);
         _;
         vm.stopPrank();
 
-        new SafeTxUtil().writeTxs(serializedTxs, filename());
+        new SafeTxUtil().writeTxs(serializedTxs, filename(who));
         delete serializedTxs;
     }
 
-    function filename() public view override returns (string memory) {
+    function filename(address who) public returns (string memory) {
         string memory root = vm.projectRoot();
-        root = string.concat(root, "/scripts/ops/V110/ethereum/txs/UpgradeAdapter");
+        root = string.concat(root, "/scripts/ops/V110/ethereum/txs/");
 
-        return string.concat(root, "-comptroller.json");
+        filecount++;
+        string memory file = string.concat("UpgradeAdapter-", filecount.toString());
+        file = string.concat(file, "-");
+        file = string.concat(file, who.toHexString());
+        file = string.concat(file, ".json");
+
+        return string.concat(root, file);
     }
 
     function run() public override {
         deployMintableLockboxes();
-        generateComptrollerTxs();
+        generateTxs();
     }
 
-    function generateComptrollerTxs() public prankAndWriteTxs(broadcastConfig.delegate) {
-        addMinterRoles();
+    function generateTxs() public {
+        addMinterRole(broadcastConfig.delegate, frxUsd, ethFrxUsdLockbox);
+        addMinterRole(0x4b45D73b83686e69d08E61105FdB7F7b51f41Bc1, sfrxUsd, ethSFrxUsdLockbox);
+
         upgradeExistingLockboxes();
     }
 
-    function addMinterRoles() public {
-        addMinterRole(frxUsd, ethFrxUsdLockbox);
-        addMinterRole(sfrxUsd, ethSFrxUsdLockbox);
-    }
-
-    function addMinterRole(address _token, address _lockbox) public {
+    function addMinterRole(address _msig, address _token, address _lockbox) public prankAndWriteTxs(_msig) {
         bytes memory data = abi.encodeCall(
             IERC20PermitPermissionedOptiMintable.addMinter,
             (_lockbox)
@@ -82,7 +89,7 @@ contract UpgradeAdapter is DeployFraxOFTProtocol {
         );
     }
 
-    function upgradeExistingLockboxes() public {
+    function upgradeExistingLockboxes() public prankAndWriteTxs(broadcastConfig.delegate) {
         upgradeExistingLockbox(ethFrxUsdLockbox, frxUsdMintableLockboxImp);
         upgradeExistingLockbox(ethSFrxUsdLockbox, sfrxUsdMintableLockboxImp);
     }

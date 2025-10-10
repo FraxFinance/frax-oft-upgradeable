ff700ca154db11e0f73bb179fd7d6647b3364e37
dhruvinparikh <imsilent93@gmail.com>
2025-10-10 20:22:12 +0000

    (chore): script to deploy mfrax and mfraxwallet

--- scripts/ops/FraxDVNTest/mainnet/tron/1_DeployMockFrax.ts
+++ scripts/ops/FraxDVNTest/mainnet/tron/1_DeployMockFrax.ts
@@ -13,80 +13,140 @@ import {
     abi as TransparentUpgradeableProxyABI,
     bytecode as TransparentUpgradeableProxyBytecode,
 } from '../../../../../out/TransparentUpgradeableProxy.sol/TransparentUpgradeableProxy.json'
+import { abi as EndpointABI } from '../../../../../out/EndpointV2.sol/EndpointV2.json'
+import {
+    abi as FraxOFTWalletUpgradeableABI,
+    bytecode as FraxOFTWalletUpgradeableBytecode,
+} from '../../../../../out/FraxOFTWalletUpgradeable.sol/FraxOFTWalletUpgradeable.json'
 
 configDotenv({ path: './.env' })
 
-// async function main() {
-//     let contract_instance = await getTronWeb().contract().new({
-//         abi: ImplementationMockArtifacts.abi,
-//         bytecode: ImplementationMockArtifacts.bytecode.object,
-//         feeLimit: 1000000000,
-//         callValue: 0,
-//         userFeePercentage: 1,
-//         originEnergyLimit: 10000000,
-//     });
-//     return utils.base58.encode58(contract_instance.address as string)
-//     // return getTronWeb().trx.getBlockByNumber(12345).then(result => {console.log(result)});
-// }
-
-// main().then(console.log).catch(console.error)
-
 async function main() {
-    const mFraxProxyAdmin = 0x8d8290d49e88D16d81C6aDf6C8774eD88762274A;
+    const mFraxProxyAdmin = 0x8d8290d49e88d16d81c6adf6c8774ed88762274a
+    const oftName = 'Mock Frax'
+    const oftSymbol = 'mFRAX'
     const configDeployerPK = process.env.PK_OFT_DEPLOYER as string
     const tronWeb = getTronWeb(configDeployerPK)
-    const deployerAddress = tronWeb.address.fromPrivateKey(configDeployerPK);
+    const deployerAddress = tronWeb.address.fromPrivateKey(configDeployerPK)
     const { allConfigs, broadcastConfig } = setUp()
-    await preDeployChecks(tronWeb, allConfigs, broadcastConfig)
+    const endpointInstance = await tronWeb.contract(EndpointABI, tronWeb.address.fromHex(broadcastConfig.endpoint))
+    await preDeployChecks(allConfigs, broadcastConfig, endpointInstance)
 
     const implementationMock = await deployContract(tronWeb, ImplementationMockABI, ImplementationMockBytecode, [])
+    console.log('implementationMock ', implementationMock.address)
 
-    const implementation = await deployContract(tronWeb, FraxOFTUpgradeableABI, FraxOFTUpgradeableBytecode, [
+    const fraxOftUpgradeable = await deployContract(tronWeb, FraxOFTUpgradeableABI, FraxOFTUpgradeableBytecode, [
         broadcastConfig.endpoint,
     ])
+
+    console.log('fraxOftUpgradeable ', fraxOftUpgradeable.address)
 
     const proxy = await deployContract(tronWeb, TransparentUpgradeableProxyABI, TransparentUpgradeableProxyBytecode, [
         implementationMock,
         deployerAddress,
         '',
     ])
+    console.log('proxy ', proxy.address)
 
-    const initializeFnSelector = tronWeb.sha3('initialize(string,string,address)').slice(0, 10);
+    const initializeFnSelector = tronWeb.sha3('initialize(string,string,address)').slice(0, 10)
 
     const encodedArgs = tronWeb.utils.abi.encodeParams(
         ['string', 'string', 'address'],
-        ["Mock Frax", "mFRAX", deployerAddress]
-    );
+        [oftName, oftSymbol, deployerAddress]
+    )
 
-    const finalData = initializeFnSelector + encodedArgs.slice(2);
+    const finalData = initializeFnSelector + encodedArgs.slice(2)
 
-    const proxyInstance = await tronWeb.contract(TransparentUpgradeableProxyABI, proxy)
     // upgradeToAndCall
-    const res = proxyInstance.upgradeToAndCall(implementation, finalData).send({
+    const upgradeToAndCallTx = await proxy.upgradeToAndCall(fraxOftUpgradeable, finalData).send({
         feeLimit: 100_000_000,
         callValue: 0,
-        tokenId: 1000036,
-        tokenValue: 100,
-        shouldPollResponse: true
-    });
+        keepTxID: true,
+        shouldPollResponse: true,
+    })
+    console.log('upgradeToAndCallTx ', upgradeToAndCallTx)
     // change admin
-    const res1 = proxyInstance.changeAdmin(mFraxProxyAdmin).send({
+    const changeProxyTx = await proxy.changeAdmin(mFraxProxyAdmin).send({
         feeLimit: 100_000_000,
         callValue: 0,
-        tokenId: 1000036,
-        tokenValue: 100,
-        shouldPollResponse: true
-    });
+        keepTxID: true,
+        shouldPollResponse: true,
+    })
+    console.log('changeProxyTx ', changeProxyTx)
     // State checks
-    // require(isStringEqual(FraxOFTUpgradeable(proxy).name(), _name), "OFT name incorrect");
-    // require(isStringEqual(FraxOFTUpgradeable(proxy).symbol(), _symbol), "OFT symbol incorrect");
-    // require(address(FraxOFTUpgradeable(proxy).endpoint()) == broadcastConfig.endpoint, "OFT endpoint incorrect");
-    // require(
-    //     EndpointV2(broadcastConfig.endpoint).delegates(proxy) == vm.addr(configDeployerPK),
-    //     "Endpoint delegate incorrect"
-    // );
-    // require(FraxOFTUpgradeable(proxy).owner() == vm.addr(configDeployerPK), "OFT owner incorrect");
+    const mockFraxOftInstance = await tronWeb.contract(FraxOFTUpgradeableABI, proxy.address as string)
 
-    // deployFraxOFTWalletUpgradeableAndProxy
+    const frxOftName = await mockFraxOftInstance.name().call()
+    console.log('frxOftName ', frxOftName)
+    if (frxOftName !== 'Mock Frax') {
+        throw new Error('OFT name is incorrect')
+    }
+
+    const frxOftSymbol = await mockFraxOftInstance.symbol().call()
+    console.log('frxOftSymbol ', frxOftSymbol)
+    if (frxOftSymbol !== 'mFRAX') {
+        throw new Error('OFT symbol is incorrect')
+    }
+
+    const endpoint = await mockFraxOftInstance.endpoint().call()
+    console.log('endpoint ', endpoint)
+    if (endpoint !== broadcastConfig.endpoint) {
+        throw new Error('OFT endpoint is incorrect')
+    }
+
+    const delegate = await endpointInstance.delegates(proxy.address).call()
+    console.log('delegate ', delegate)
+    console.log('deployerAddress ', deployerAddress)
+    if (delegate !== deployerAddress) {
+        throw new Error('Endpoint delegate is incorrect')
+    }
+
+    const owner = await proxy.owner().call()
+    console.log('owner ', owner)
+    if (owner !== deployerAddress) {
+        throw new Error('OFT owner is incorrect')
+    }
+
+    const fraxOFTWalletUpgradeable = await deployContract(
+        tronWeb,
+        FraxOFTWalletUpgradeableABI,
+        FraxOFTWalletUpgradeableBytecode,
+        []
+    )
+
+    console.log('fraxOFTWalletUpgradeable ', fraxOFTWalletUpgradeable.address)
+
+    const walletProxy = await deployContract(
+        tronWeb,
+        TransparentUpgradeableProxyABI,
+        TransparentUpgradeableProxyBytecode,
+        [implementationMock, deployerAddress, '']
+    )
+    console.log('walletProxy ', walletProxy.address)
+
+    const walletInitializeFnSelector = tronWeb.sha3('initialize(address)').slice(0, 10)
+
+    const walletEncodedArgs = tronWeb.utils.abi.encodeParams(['address'], [deployerAddress])
+
+    const walletFinalData = walletInitializeFnSelector + walletEncodedArgs.slice(2)
+
+    // upgradeToAndCall
+    const walletUpgradeToAndCallTx = await walletProxy
+        .upgradeToAndCall(fraxOFTWalletUpgradeable, walletFinalData)
+        .send({
+            feeLimit: 100_000_000,
+            callValue: 0,
+            keepTxID: true,
+            shouldPollResponse: true,
+        })
+    console.log('walletUpgradeToAndCallTx ', walletUpgradeToAndCallTx)
+    // change admin
+    const walletchangeProxyTx = await walletProxy.changeAdmin(mFraxProxyAdmin).send({
+        feeLimit: 100_000_000,
+        callValue: 0,
+        keepTxID: true,
+        shouldPollResponse: true,
+    })
+    console.log('walletchangeProxyTx ', walletchangeProxyTx)
 }
 main().then(console.log).catch(console.error)
--- scripts/ops/FraxDVNTest/mainnet/tron/tron-helper.ts
+++ scripts/ops/FraxDVNTest/mainnet/tron/tron-helper.ts
@@ -1,28 +1,27 @@
-import { TronWeb } from "tronweb"
-import L0ConfigJson from "../../../../L0Config.json";
-import { L0Config } from "./types";
-import { abi as EndpointABI } from "../../../../../out/EndpointV2.sol/EndpointV2.json";
+import { TronWeb, Types as TronWebTypes, Types } from 'tronweb'
+import L0ConfigJson from '../../../../L0Config.json'
+import { L0Config } from './types'
 
-export function setUp(): { broadcastConfig: L0Config, allConfigs: L0Config[] } {
+export function setUp(): { broadcastConfig: L0Config; allConfigs: L0Config[] } {
     // The final array
-    const allConfigs: L0Config[] = [];
+    const allConfigs: L0Config[] = []
 
-    let broadcastConfig: L0Config | null = null;
+    let broadcastConfig: L0Config | null = null
 
     // Push everything from Legacy
     if (Array.isArray(L0ConfigJson.Legacy)) {
-        allConfigs.push(...L0ConfigJson.Legacy);
+        allConfigs.push(...L0ConfigJson.Legacy)
     }
 
     // Push from Proxy, with filtering on chainid
     if (Array.isArray(L0ConfigJson.Proxy)) {
-        const filtered = L0ConfigJson.Proxy.filter(cfg => ![1, 81457, 1088, 8453].includes(cfg.chainid));
-        allConfigs.push(...filtered);
+        const filtered = L0ConfigJson.Proxy.filter((cfg) => ![1, 81457, 1088, 8453].includes(cfg.chainid))
+        allConfigs.push(...filtered)
     }
 
     // Push everything from Non-EVM
-    if (Array.isArray(L0ConfigJson["Non-EVM"])) {
-        allConfigs.push(...L0ConfigJson["Non-EVM"]);
+    if (Array.isArray(L0ConfigJson['Non-EVM'])) {
+        allConfigs.push(...L0ConfigJson['Non-EVM'])
     }
 
     for (let e = 0; e < allConfigs.length; e++) {
@@ -34,35 +33,51 @@ export function setUp(): { broadcastConfig: L0Config, allConfigs: L0Config[] } {
     }
 
     if (broadcastConfig === null) {
-        throw new Error("broadcast config not found. Please check tronbox and L0 config files");
+        throw new Error('broadcast config not found. Please check tronbox and L0 config files')
     }
 
     return { broadcastConfig, allConfigs }
 }
 
-export async function preDeployChecks(tronWeb: TronWeb, _allConfigs: L0Config[], _broadcastConfig: L0Config) {
-    const endpointInstance = await tronWeb.contract(EndpointABI, tronWeb.address.fromHex(_broadcastConfig.endpoint));
+export async function preDeployChecks(
+    _allConfigs: L0Config[],
+    _broadcastConfig: L0Config,
+    endpointInstance: Types.ContractInstance<Types.ContractAbiInterface>
+) {
     for (const config of _allConfigs) {
         if (config.eid === _broadcastConfig.eid) continue
 
         // L0 team has not setup defaultSendLibrary and defaultReceiveLibrary on tron mainnet for
         // 30370 (plume), botanix (30376), katana (30375), hyperliquid (30367), movement (30325)
         // and aptos (30108)
-        if (config.eid === 30370 || config.eid === 30376 || config.eid === 30375 || config.eid === 30367 || config.eid === 30325 || config.eid === 30108) continue
-        const eid = Number(config.eid);
+        if (
+            config.eid === 30370 ||
+            config.eid === 30376 ||
+            config.eid === 30375 ||
+            config.eid === 30367 ||
+            config.eid === 30325 ||
+            config.eid === 30108
+        )
+            continue
+        const eid = Number(config.eid)
         try {
-            const isSupported = await endpointInstance.isSupportedEid(eid).call();
+            const isSupported = await endpointInstance.isSupportedEid(eid).call()
             console.log(isSupported)
             if (!isSupported) {
-                throw new Error(`L0 team required to setup defaultSendLibrary and defaultReceiveLibrary for ${eid}`);
+                throw new Error(`L0 team required to setup defaultSendLibrary and defaultReceiveLibrary for ${eid}`)
             }
         } catch (error) {
-            console.error(error);
+            console.error(error)
         }
     }
 }
 
-export async function deployContract(tronWeb: TronWeb, abi: any, bytecode: any, parameters: any[]): Promise<string> {
+export async function deployContract(
+    tronWeb: TronWeb,
+    abi: any,
+    bytecode: any,
+    parameters: any[]
+): Promise<TronWebTypes.ContractInstance<TronWebTypes.ContractAbiInterface>> {
     const instance = await tronWeb.contract().new({
         abi: abi,
         bytecode: bytecode.object,
@@ -71,6 +86,6 @@ export async function deployContract(tronWeb: TronWeb, abi: any, bytecode: any, 
         userFeePercentage: 1,
         originEnergyLimit: 10000000,
         parameters,
-    });
-    return instance.address as string
+    })
+    return instance
 }
